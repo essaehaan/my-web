ee7c620c96a5254582d4d367b221c5eff39df4f6
essaehaan <essa.nagari2@gmail.com>
2024-08-29 05:16:37 +0500

    Resume page

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+"use client"
+import {
+FaHtml5,
+FaCss3,
+FaJs,
+FaReact,
+FaFigma,
+FaNodeJs,
+}
+from "react-icons/fa"
+import { SiTailwindcss, Sinextdotjs } from "react-icons/si"
+const about ={
+  title: 'About me',
+  description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem." ,
+  info:[
+
+      {
+        fieldName: "Name",
+        fieldValue: "Essa Ehaan",
+      },
+      {
+        fieldName: "Phone No",
+        fieldValue: "(92)332-3317898",
+      },
+      {
+        fieldName: "Email",
+        fieldValue: "developeressaehaan@gmail.com",
+      },
+      {
+        fieldName: "LinkedIn",
+        fieldValue: "Essaehaan",
+      },
+      {
+        fieldName: "Nationality",
+        fieldValue: "Pakistani",
+      },
+      {
+        fieldName: "Experiance",
+        fieldValue: "Essaehaan",
+      },
+
+  ]
+
+}
 
 const Resume = () => {
   return (
     <div>
-      Resume
+     
     </div>
   )
 }
