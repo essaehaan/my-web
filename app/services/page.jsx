d19a78fb8eb7caf72e336b262198068a4b131dc8
essaehaan <essa.nagari2@gmail.com>
2024-08-29 05:15:32 +0500

    Resume

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -50,20 +50,24 @@ const Services = () => {
         className=" grid grid-cols-1 md:grid-cols-2 gap-[60px]">
           { services.map((service, index) => {
             return(
-              <div key={index}>
+              <div key={index}
+              className="flex-1 flex flex-col justify-center gap-6 group">
                 <div className="w-full flex justify-between items-center">
                 <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover  transition-all duration-500">
                   {service.num}
                   </div>
                 <Link href={service.href}
-                className="w-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45"
+                className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45"
                 >
                   <BsArrowDownRight className="text-primary text-3xl"/>
                 </Link>
 
-              </div> 
-              <h2>{service.title}</h2>
-              <p>{service.description}</p>
+              </div>
+              <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500">
+                {service.title}
+
+              </h2>
+              <p className="text-white/60">{service.description}</p>
               <div className="border-b border-white/70 w-full"></div>
               </div>
             )
